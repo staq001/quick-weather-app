0cf2d2d764b09f708ee33b7120d0684f7043254e
staq001 <dame.hush@gmail.com>
2022-12-12 15:41:36 +0100

    Remove unnecesssary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript file is loaded");
-
 // fetch("http://localhost:3000/weather?address=").then((response) => {
 //   response.json().then((data) => {
 //     if (data.error) {
