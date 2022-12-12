16d5d44cb45a861aa30d5df9be0342d0103b5dc5
staq001 <dame.hush@gmail.com>
2022-12-12 19:41:28 +0100

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -26,7 +26,8 @@ weatherForm.addEventListener("submit", function (e) {
   const location = search.value;
   const realLocation = location.toString();
 
-  fetch(`http://localhost:3000/weather?address=${realLocation}`).then(
+  fetch(`weather?address=${realLocation}`).then(
+    // for heroku
     (response) =>
       response.json().then((data) => {
         // console.log(data.forecast);
